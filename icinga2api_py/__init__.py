7dbb1e98ba3930af646aeaa3e26de7ecdeeb683d
TimL20 <tim.le20tl@gmail.com>
2020-02-13 15:07:04 +0100

    Updated setup.py and author/copyright notice
    Real author name
    Explicit "packages" in setup.py
    Added "python_requires" because of typing hints introduced in a commit some weeks ago
    Added "extras_require" in setup.py
    Added intended audience as setup.py metadata
    Added keywords to setup.py
    Added project URL(s) to setup.py

--- icinga2api_py/__init__.py
+++ icinga2api_py/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+__version__ = "0.6.26"
+__author__ = "Tim Lehner"
+
 from .api import API
 from .clients import Client, StreamClient
 from .simple_oo import Icinga2
 from .results import ResultSet, ResultsFromResponse, ResultsFromRequest, ResultList, Result
-
-__version__ = "0.6.25"
--- setup.py
+++ setup.py
@@ -8,19 +8,30 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="icinga2api_py",
-	version="0.6.25",
-	author="TimL20",
+	version="0.6.26",
+	author="Tim Lehner",
 	description="Icinga2 API client for Python",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	license="BSD 3-Clause License",
-	packages=setuptools.find_packages(),
+	packages=["icinga2api_py"],
+	python_requires=">=3.5",
 	install_requires=['requests'],
 	tests_require=['requests', 'pytest'],
+	extras_require={
+		"test": ["pytest"],
+	},
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"Operating System :: OS Independent",
 		"Development Status :: 3 - Alpha",
-		"License :: OSI Approved :: BSD License"
+		"License :: OSI Approved :: BSD License",
+		"Intended Audience :: Developers",
 	],
+	keywords="icinga2 api library",
+	url="https://github.com/TimL20/icinga2api_py",
+	project_urls={
+		"Source": "https://github.com/TimL20/icinga2api_py",
+		"Bug Reports": "https://github.com/TimL20/icinga2api_py/issues",
+	},
 )
